691a2a1f77417dad6ae326585eb54e2c9bb9a71b
richiecodes <oyelekerichard@yahoo.com>
2020-10-28 09:21:26 +0100

    COMMIT AFTER REPORT FORMAT CHANGED ON NERC REPORT

--- src/main/java/com/crowninteractive/net/nercreport/controller/NERCController.java
+++ src/main/java/com/crowninteractive/net/nercreport/controller/NERCController.java
@@ -8,7 +8,6 @@ package com.crowninteractive.net.nercreport.controller;
 import com.crowninteractive.net.nercreport.dto.BaseResponse;
 import com.crowninteractive.net.nercreport.exception.NercReportException;
 import com.crowninteractive.net.nercreport.jms.ReportReceiver;
-import com.crowninteractive.net.nercreport.service.ReportService;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -34,8 +33,7 @@ public class NERCController {
 
     @Autowired
     private ReportReceiver reportReceiver;
-    @Autowired
-    private ReportService reportService;
+    
     BaseResponse awe;
 
     @GetMapping("test")
@@ -74,7 +72,7 @@ public class NERCController {
 //        return new ResponseEntity("NERC Report Request Received And Is Been Processed", HttpStatus.OK);
         return new Gson().toJson(awe);
     }
-
+    
     @GetMapping("start_report_v2")
     public String startReportV2(@RequestParam("from") String from,
             @RequestParam("to") String to,
@@ -93,12 +91,12 @@ public class NERCController {
     @GetMapping("extra_data")
     public ResponseEntity extraData(@RequestParam("ticketId") int ticketId) throws ParseException {
         try {
-            reportService.getComplaintDetailsV1(ticketId);
+            reportReceiver.getComplaintDetailsV1(ticketId);
         } catch (Exception ex) {
             Logger.getLogger(NERCController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        return new ResponseEntity(reportService.getComplaintDetailsV1(ticketId), HttpStatus.OK);
+        return new ResponseEntity(reportReceiver.getComplaintDetailsV1(ticketId), HttpStatus.OK);
     }
 
 }
